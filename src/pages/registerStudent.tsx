95ad38c1960fabc683aa6acf273ccff31deb6ddd
ValbertMartins <valbertclash@gmail.com>
2023-04-16 11:11:50 -0300

    fix: removed page registerstudent

--- src/pages/registerStudent.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router"
-
-const RegisterStudent = () => {
-  const { pathname, asPath } = useRouter()
-
-  return <div>RegisterStudent</div>
-}
-
-export default RegisterStudent
