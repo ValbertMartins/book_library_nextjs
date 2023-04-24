d12e42f7f97f176370a6e71cfba15deee771a655
ValbertMartins <valbertclash@gmail.com>
2023-04-23 22:08:35 -0300

    fix: final version 1 to table layout

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -11,27 +11,24 @@ interface Props {
 const Table = ({ sourceData, setStudentList }: Props) => {
   console.log(sourceData)
   return (
-    <section className="border-x border-t border-zinc-100  bg-white rounded-lg overflow-hidden grid grid-cols-10   bg-primary-color">
-      <div className="border-b-[1px] border-r-[1px] bg-primary-color border-zinc-100 pl-4 py-4 font-bold col-start-1 col-end-3">
+    <section className="border-x border-t border-zinc-100  bg-white rounded-lg  grid grid-cols-12 overflow-x-scroll md:overflow-hidden  bg-primary-color">
+      <div className="border-b-[1px] border-r-[1px] bg-primary-color border-zinc-100 pl-4 py-4 font-bold col-start-1 col-end-5">
         Nome
       </div>
-
       <div className="border-b-[1px] bg-primary-color border-r-[1px] border-zinc-100 pl-4 py-4 font-bold">
         Gênero
       </div>
-
       <div className="border-b-[1px] bg-primary-color border-r-[1px] border-zinc-100 pl-4 py-4 font-bold">
         Série
       </div>
-
       <div className="border-b-[1px] bg-primary-color border-r-[1px] border-zinc-100 pl-4 py-4 font-bold">
         Turma
       </div>
 
-      <div className="border-b-[1px] bg-primary-color border-r-[1px] border-zinc-100 pl-4 py-4 font-bold col-start-6 col-end-8 ">
+      <div className="border-b-[1px] bg-primary-color border-r-[1px] border-zinc-100 pl-4 py-4 font-bold col-start-8 col-end-10">
         Livros coletados
       </div>
-      <div className="border-b-[1px] bg-primary-color border-r-[1px] border-zinc-100 pl-4 py-4 font-bold col-start-8 col-end-10">
+      <div className="border-b-[1px] bg-primary-color border-r-[1px] border-zinc-100 pl-4 py-4 font-bold col-start-10 col-end-12">
         Livros devolvidos
       </div>
 
@@ -40,11 +37,8 @@ const Table = ({ sourceData, setStudentList }: Props) => {
       </div>
 
       {sourceData.map(student => (
-        <Fragment
-          key={student.id}
-          // className="grid grid-cols-10  cursor-pointer  group"
-        >
-          <div className=" border-b-[1px] group-hover:bg-primary-color col-start-1 col-end-3">
+        <Fragment key={student.id}>
+          <div className=" border-b-[1px] group-hover:bg-primary-color col-start-1 col-end-5">
             <p className="my-4 ml-4">{student.name}</p>
           </div>
           <div className=" border-b-[1px] group-hover:bg-primary-color">
@@ -57,11 +51,10 @@ const Table = ({ sourceData, setStudentList }: Props) => {
             <p className="my-4 ml-4">{student.class}</p>
           </div>
 
-          <div className=" border-b-[1px] group-hover:bg-primary-color col-start-6 col-end-8">
+          <div className=" border-b-[1px] group-hover:bg-primary-color col-start-8 col-end-10">
             <p className="my-4 ml-4">{student.studentProgress.returned_books} </p>
           </div>
-
-          <div className=" border-b-[1px] group-hover:bg-primary-color  col-start-8 col-end-10">
+          <div className=" border-b-[1px] group-hover:bg-primary-color  col-start-10 col-end-12">
             <p className="my-4 ml-4">{student.studentProgress.collected_books}</p>
           </div>
 
