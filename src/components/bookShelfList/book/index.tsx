fec8c1058a39bb5a0219625004657061cf75460f
ValbertMartins <valbertclash@gmail.com>
2023-06-05 22:35:56 -0300

    styles: blur in loading books

---
+++ src/components/bookShelfList/book/index.tsx
@@ -0,0 +1,52 @@
+import { Book } from "@/interfaces"
+import Image from "next/image"
+import React, { Dispatch, SetStateAction, useState } from "react"
+
+const coverPreviewPlaceholder = "/book_cover_placeholder.png"
+
+interface Props {
+  book: Book
+  setBook: Dispatch<SetStateAction<Book | undefined>>
+  setOpenModalBookDetails: Dispatch<SetStateAction<boolean>>
+}
+const Book = ({ book, setBook, setOpenModalBookDetails }: Props) => {
+  const [coverLoading, setCoverLoading] = useState(true)
+  return (
+    <div
+      onClick={() => {
+        setOpenModalBookDetails(true)
+        setBook(book)
+      }}
+      key={book.id}
+      className="flex flex-col cursor-pointer"
+    >
+      <div className=" rounded-xl overflow-hidden aspect-[1/1.3]">
+        <Image
+          className={`object-cover h-full w-full  duration-700 ease-in-out ${
+            coverLoading ? "blur-xl scale-110" : " blur-0 scale-100"
+          }`}
+          width={300}
+          height={100}
+          priority
+          src={book.cover ? book.cover : coverPreviewPlaceholder}
+          onLoadingComplete={() => setCoverLoading(false)}
+          alt={`Capa do livro ${book.name} `}
+        />
+      </div>
+
+      <div className="mt-2">
+        <p className="font-bold px-1 text-sm">{book.name}</p>
+        <p className="px-1 text-xs text-slate-400 my-1">Quantidade: {book.quantity}</p>
+        <p
+          className={`px-1 text-xs ${
+            book.quantity_available === 0 ? "text-red-500" : " text-slate-400"
+          }`}
+        >
+          Disponível: {book.quantity_available}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Book
--- src/components/bookShelfList/index.tsx
+++ src/components/bookShelfList/index.tsx
@@ -1,8 +1,6 @@
-import { Book } from "@/interfaces"
-import Image from "next/image"
+import Book from "./book"
 import { Dispatch, SetStateAction, useState } from "react"
 import { BookDetails } from "../bookDetails"
-const coverPreviewPlaceholder = "/book_cover_placeholder.png"
 
 interface Props {
   bookList: Book[]
@@ -10,47 +8,17 @@ interface Props {
 }
 
 const BookList = ({ bookList, setBookList }: Props) => {
-  const [imagesLoading, setImagesLoading] = useState(true)
   const [openModalBookDetails, setOpenModalBookDetails] = useState(false)
   const [book, setBook] = useState<Book | undefined>(undefined)
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8  md:gap-x-4 md:gap-y-10 xl:grid-cols-5  overflow-y-scroll max-h-[65vh] min-h-[65vh] 2xl:gap-x-2">
       {bookList.map(book => (
-        <div
-          onClick={() => {
-            setOpenModalBookDetails(true)
-            setBook(book)
-          }}
-          key={book.id}
-          className="flex flex-col cursor-pointer"
-        >
-          <div className=" rounded-xl overflow-hidden ">
-            <Image
-              className={`object-cover h-full w-full transition-all ${
-                imagesLoading ? "blur-md scale-100" : "grayscale-0 blur-0 scale-100"
-              }`}
-              width={300}
-              height={100}
-              priority
-              src={book.cover ? book.cover : coverPreviewPlaceholder}
-              onLoadingComplete={() => setImagesLoading(false)}
-              alt={`Capa do livro ${book.name} `}
-            />
-          </div>
-
-          <div className="mt-2">
-            <p className="font-bold px-1 text-sm">{book.name}</p>
-            <p className="px-1 text-xs text-slate-400 my-1">Quantidade: {book.quantity}</p>
-            <p
-              className={`px-1 text-xs ${
-                book.quantity_available === 0 ? "text-red-500" : " text-slate-400"
-              }`}
-            >
-              Disponível: {book.quantity_available}
-            </p>
-          </div>
-        </div>
+        <Book
+          book={book}
+          setBook={setBook}
+          setOpenModalBookDetails={setOpenModalBookDetails}
+        />
       ))}
       {bookList.length == 0 && <p>Nenhum livro encontrado</p>}
 
